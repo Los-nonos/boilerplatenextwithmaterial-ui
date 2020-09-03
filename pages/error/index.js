f25101d52ec679ee64664c1c1bedd896257a150e
cristianvena18 <cristiandamianvena@gmail.com>
2020-09-03 20:58:21 -0300

    redirect from error page fixed

---
+++ pages/error/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ErrorIndex extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Error Page</h2>
+            </div>
+        )
+    }
+}
+
+export default ErrorIndex;
